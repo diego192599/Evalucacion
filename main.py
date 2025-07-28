9c82b6e7d287e20aa07bc7283e4859548318f300
diego192599 <di.cramire@udeo.edu.gt>
2025-07-28 10:22:20 -0600

    creacion de metodos ingresar datos y mostrar informacion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+empledos={}
 class Empleado:
-    def __init__(self,nombre,departamento,trabajo):
+    def __init__(self,codigo,nombre,departamento,trabajo):
+        self.codigo=codigo
         self.nombre=nombre
         self.departamento=departamento
         self.trabajo=trabajo
@@ -7,7 +9,22 @@ class Empleado:
         cantidad=int(input("Ingrese la cantida de trabajadores ingresara: "))
         for i in range(cantidad):
             print(f"\n Trabajador #{i+1}")
-            nombre=input("Ingrese el nombre del trabajador")
+            codigo=input("Ingrese su codigo: ")
+            nombre=input("Ingrese el nombre del trabajador: ")
+            departamento=input("Ingrese sus departamento: ")
+            trabajo=int(input("Ingrese los años trabajados en la empresa: "))
+            if trabajo<=0:
+                print("Los años trabajados no son validos")
+            else:
+                print("Datos guardados correctamente")
+
+        empledos = {
+            "codigo": {
+                "nombre": nombre,
+                "departamento": departamento,
+                "trabajo": trabajo
+            }
+        }
     def mostrar_informacion(self):
         print("\n La informacion de los trabajadores son: ")
         print(f"Nombre: {self.nombre}")
