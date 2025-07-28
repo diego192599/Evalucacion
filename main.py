a23949667a3a5ed348cb0fd3388676637f0c299b
diego192599 <di.cramire@udeo.edu.gt>
2025-07-28 10:14:50 -0600

    creacion de clase empleado

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Menu")
+class Empleado:
+    def __init__(self,nombre,departamento,trabajo):
+        self.nombre=nombre
+        self.departamento=departamento
+        self.trabajo=trabajo
+    def ingresar_datos(self):
+        cantidad=int(input("Ingrese la cantida de trabajadores ingresara: "))
+        for i in range(cantidad):
+            print(f"\n Trabajador #{i+1}")
+            nombre=input("Ingrese el nombre del trabajador")
+    def mostrar_informacion(self):
+        print("\n La informacion de los trabajadores son: ")
+        print(f"Nombre: {self.nombre}")
+        print(f"Departamento: {self.departamento}")
+        print(f"Años trabajados son: {self.trabajo}")
